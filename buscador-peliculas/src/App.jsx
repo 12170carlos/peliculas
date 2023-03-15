546e3173164d93dfbda1e8c35ab177c7b701e859
Carlos Alvarado <pt002014388@idat.edu.pe>
2023-03-15 15:21:34 -0500

    Feat: Debounce in App component done in order to improve the performance

--- buscador-peliculas/src/App.jsx
+++ buscador-peliculas/src/App.jsx
@@ -1,99 +1,102 @@
 import "./App.css";
-import { useEffect, useState, useRef } from "react"; //referencia mutable, que persiste en todo el ciclo de vida del componente
+import { useEffect, useState, useRef, useCallback } from "react"; //referencia mutable, que persiste en todo el ciclo de vida del componente
 import { Movies } from "./Components/movies";
-
 import { useMovies } from "./hooks/useMovies";
-import { render } from "react-dom";
+import debounce from "just-debounce-it";
 
-
-function useSearch () {
-  const [search, updateSearch] = useState('');
+function useSearch() {
+  const [search, updateSearch] = useState("");
   const [error, setError] = useState(null);
-  const isFirstInput = useRef(true)
+  const isFirstInput = useRef(true);
 
   const firstInput = () => {
     if (isFirstInput.current) {
-      isFirstInput.current = search === ''
-      return
+      isFirstInput.current = search === "";
+      return;
     }
-  }
+  };
 
   useEffect(() => {
-   
-    firstInput()
-    
-    if (search === ''){
-      
-      setError('EL campo no debe estar vacío')
-      return
+    firstInput();
+
+    if (search === "") {
+      setError("EL campo no debe estar vacío");
+      return;
     }
 
     if (search.match(/^\d+$/)) {
-      setError('No se puede buscar una pelicula con número');
-      return
+      setError("No se puede buscar una pelicula con número");
+      return;
     }
 
     if (search.length < 3) {
-      
-      setError('La búsqueda debe tener al menos 3️⃣ caracteres');
-      return
+      setError("La búsqueda debe tener al menos 3️⃣ caracteres");
+      return;
     }
 
-    setError(null)
+    setError(null);
+  }, [search]);
 
-  }, [search])
-
-  return { search, updateSearch, error }
+  return { search, updateSearch, error };
 }
 
 function App() {
-
-  const [ sort, setSort ]  = useState(false)
+  const [sort, setSort] = useState(false);
   const { search, updateSearch, error } = useSearch();
-  const  { movies: templateMovies, loading, getMovies } = useMovies({ search, sort });
-  
+  const {
+    movies: templateMovies,
+    loading,
+    getMovies,
+  } = useMovies({ search, sort });
+
+  const debounceGetMovies = useCallback(
+    debounce((search) => {
+      console.log("Search", search);
+      getMovies({ search });
+    }, 300),
+    [getMovies]
+  );
 
   const handleSort = () => {
-    setSort(!sort)
-  }
-  
+    setSort(!sort);
+  };
+
   const handleSubmit = (event) => {
-    event.preventDefault()
-       
-    getMovies({ search })
-  }
+    event.preventDefault();
+
+    getMovies({ search });
+  };
 
   const handleChange = (event) => {
-    const newQuery = event.target.value
-    if (newQuery.startsWith(' ')) return
-    updateSearch(event.target.value)
-    
-  }
+    const newQuery = event.target.value;
 
-  
+    if (newQuery.startsWith(" ")) return;
+
+    updateSearch(newQuery);
+    debounceGetMovies(newQuery);
+  };
 
   return (
     <div className="page">
       <header>
         <form className="form" onSubmit={handleSubmit}>
-          <input onChange={handleChange} value={search} name="search" placeholder="Avengers, Star Wars, The Matriz..." />
-          
-          <input type='checkbox' onChange={handleSort} checked={sort}></input>
-          <button 
-            disabled={error}
-            type="submit">Buscar</button>
-        </form>
-        {error && <p style={ {color: 'red'} } > {error}</p>}
-      </header>
-    
-      <main>
-        {
-          loading ? <p>Cargando...</p>
-          :
+          <input
+            onChange={handleChange}
+            value={search}
+            name="search"
+            placeholder="Avengers, Star Wars, The Matriz..."
+          />
 
-        <Movies movies={templateMovies} />
-        }
-        
+          <input type="checkbox" onChange={handleSort} checked={sort}></input>
+          <button disabled={error} type="submit">
+            Buscar
+          </button>
+        </form>
+        {error && <p style={{ color: "red" }}> {error}</p>}
+      </header>
+
+      <main>
+        {loading ? <p>Cargando...</p> : <Movies movies={templateMovies} />}
       </main>
     </div>
   );
